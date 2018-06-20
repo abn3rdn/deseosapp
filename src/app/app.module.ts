a5a0b3ed121ed83e4b0ed692c5f6caf2757b06fb
Abner Rojas <ar@smartlending.mx>
2018-06-20 16:20:57 -0500

    solucion imports en app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 import { PendientesComponent } from '../pages/pendientes/pendientes.component';
 import { TerminadosComponent } from '../pages/terminados/terminados.component';
 
@@ -17,7 +20,8 @@ import { TerminadosComponent } from '../pages/terminados/terminados.component';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserModule 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -27,6 +31,10 @@ import { TerminadosComponent } from '../pages/terminados/terminados.component';
     PendientesComponent,
     TabsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
 })
 export class AppModule {}
--- src/pages/pendientes/pendientes.component.ts
+++ src/pages/pendientes/pendientes.component.ts
@@ -7,5 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class PendientesComponent implements OnInit {
   constructor() {  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('init pendientes');
+  }
 }
